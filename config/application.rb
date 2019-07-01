ad2e68a02ac8f219be3ccbcb5aab166579621fe4
Sara McCombs <theSaraMcCombs@gmail.com>
2019-07-01 16:16:46 -0400

    setup for heroku part 3

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module ScienceTots
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = true
   end
 end
 
-config.assets.initialize_on_precompile = true
+
