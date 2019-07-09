61c88be879c1095f8503783011a52999d5eafca0
Erica42 <erica.forget@gmail.com>
2019-07-09 18:56:58 -0500

    add show to tutorials controller

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -2,4 +2,10 @@ class TutorialsController < ApplicationController
   def index
     @tutorials = Tutorial.all.order(created_at: :desc)
   end
+  def show
+    @tutorials = Tutorial.all.order(created_at: :desc)
+    @tutorial = Tutorial.find_by(id: params[:id])
+
+
+  end
 end
